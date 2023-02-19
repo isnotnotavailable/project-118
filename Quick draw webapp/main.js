58d6454ee08223415656d626484d550de43fb1b3
isnotnotavailable <87622374+isnotnotavailable@users.noreply.github.com>
2023-02-19 14:38:37 -0500

    Add files via upload

---
+++ Quick draw webapp/main.js
@@ -0,0 +1,12 @@
+quick_draw_data_set=["aircraft carrier","airaplane","alarm clock","ambulance"]
+
+function Setup() {
+    Canvas = createCanvas(200,200);
+    Canvas.center();
+    background("white");
+}
+
+function Clear_canvas() {
+    background("white");
+}
+
